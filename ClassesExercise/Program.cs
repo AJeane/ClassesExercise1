f437d74e06d30fb0c73833250f9e2c9f2c4173aa
AJeane <ajeane1924@gmail.com>
2021-06-28 20:36:33 -0500

    initial commit

---
+++ ClassesExercise/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassesExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+            myCar.Make = "Nissan";
+            myCar.Model = "Skyline";
+            myCar.Year = 1999;
+
+
+            var ferrari = new Car()
+            {
+                Make = "Ferrari",
+                Model = "Ferrari 458",
+                Year = 2015
+            };
+
+            var lamborghini = new Car("Lamborghini", "Egoista", 2013);
+
+            var carList = new List<Car>() { myCar, ferrari, lamborghini};
+
+            foreach(var vehicle in carList)
+            {
+                Console.WriteLine($"{vehicle.Make} {vehicle.Model} {vehicle.Year}");
+            }
+
+
+        }
+
+    }
+}
